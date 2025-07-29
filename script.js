bef2290fc4d5c68bbd4392e5bd47c28990f6993a
sapan <sapansaxena2@gmail.com>
2025-07-29 13:28:50 +0530

    add few lines in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+// Variables
+let name = "Sapan";
+const age = 25;
+
+// Function
+function greet(user) {
+    console.log("Hello, " + user);
+}
+
+greet(name);
+
+// Array
+const fruits = ["apple", "banana", "cherry"];
+fruits.forEach(fruit => console.log(fruit));
+
+console.log("JavaScript is working!");
